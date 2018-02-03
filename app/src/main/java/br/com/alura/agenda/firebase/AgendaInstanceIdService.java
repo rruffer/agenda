44dc267faa6a53d8e83d7670a220d189374f8d34
rruffer <alucard@22>
2018-02-03 10:33:38 -0200

    Adicionando firebase ao projeto.

---
+++ app/src/main/java/br/com/alura/agenda/firebase/AgendaInstanceIdService.java
@@ -0,0 +1,29 @@
+package br.com.alura.agenda.firebase;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by rruffer on 30/01/2018.
+ */
+
+public class AgendaInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Token Firebase", "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        enviaTokenParaServidor(refreshedToken);
+    }
+
+    private void enviaTokenParaServidor(String refreshedToken) {
+    }
+
+}
